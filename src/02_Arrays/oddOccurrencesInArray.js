fee84cf5d6271e05ab18b60e7392406f9f141aed
Chihab ASRIH <chihab.asrih@gmail.com>
2018-04-07 13:24:23 +0200

    Update oddOccurrencesInArray.js

--- src/02_Arrays/oddOccurrencesInArray.js
+++ src/02_Arrays/oddOccurrencesInArray.js
@@ -11,3 +11,31 @@ function solution(A) {
 }
 
 console.log(solution([9,8,7,9,7,8,6]));
+
+// my solution
+function mySolution(A) {
+    let N = A.length;
+    if (N === 0 ||  N > 1000000) {
+        return;
+    }
+    
+    if (N===1){
+        return A[0];
+    }
+    
+    A.sort(function(a, b){
+        return a - b;
+    });
+    console.log(A);
+    
+    for (let i=0; i < N-1 ; i+=2){
+        if (A[i] < 1 || A[i] > 1000000000) {
+            return;
+        }
+        
+        if( A[i] !== A[i+1]){
+            return A[i];
+        }
+    }
+    return A[N-1];
+}
