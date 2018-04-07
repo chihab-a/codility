79d9b5ddeb70f5d601be2af0c694f38e29bb7dd3
Chihab ASRIH <chihab.asrih@gmail.com>
2018-04-07 15:03:01 +0200

    Update tapeEquilibrium.js

--- src/03_TimeComplexity/tapeEquilibrium.js
+++ src/03_TimeComplexity/tapeEquilibrium.js
@@ -2,34 +2,37 @@
 // console.log('this is a debug message');
 
 function solution(A) {
-    // write your code in JavaScript (Node.js 4.0.0)
-    if(A.length == 1){
-    	return A[0];
-    }else if (A.length == 0){
-    	return 0;
+    let aLength = A.length;
+    
+    if(aLength === 1){
+        return A[0];
     }
-    var minimum = 100000;
-    // var sums = new Array(A.length-1);
-    // for (var i = 0; i < Math.ceil(A.length/2); i++) {
-    // 	var top = A[i];
-    // 	var last = A[A.length - 1 - i];
-    // 	sums[i] = sums[i - 1] + 
-    // }
-    var sum = ( acc, cur ) => acc + cur;
-
-    var sumTotal = A.reduce(sum,0);
-    console.log("sumTotal "+ sumTotal)
-    var leftSum = 0;
-    for (var i = 0; i <= A.length - 1; i++) {
-    	leftSum += A[i];
-    	var rightSum = sumTotal - leftSum;
-    	var diff = Math.abs(leftSum - rightSum);
-    	if(diff < minimum) {
-    		minimum = diff;
-    	}
-    	
+    
+    if (aLength === 0){
+        return 0;
     }
-    return minimum;
+    
+    if (aLength < 2 || aLength > 100000) {
+        return;
+    }
+    
+    const sum = (accumulator, currentValue) => accumulator + currentValue;
+    const sumTotal = A.reduce(sum, 0);
+    
+    let minimalDifference = 100000;
+    let leftSum = 0;
+    
+    for (let i=0; i < aLength-1; i++) {
+        leftSum += A[i];
+        const rightSum = sumTotal - leftSum;
+        const absDiff = Math.abs(leftSum - rightSum);
+        
+        if(absDiff < minimalDifference) {
+            minimalDifference = absDiff;
+        }
+    }
+    
+    return minimalDifference;
 }
 
 console.log(solution([3, 1, 2, 4, 3]));
